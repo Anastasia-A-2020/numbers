80af3ee0dd60ad376103241d727f036c132444c1
Anastasia Aleksandrova <anastasiayurievnaaleksandrova@gmail.com>
2024-06-17 13:00:49 +0300

    user numbers are stored in an array

--- src/Main.java
+++ src/Main.java
@@ -1,35 +1,47 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
 
 public class Main {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean isRunning = true;
+        System.out.println("Let the game begin!!!");
+
+        ArrayList<Integer> userNumbersList = new ArrayList<>();
 
         while (isRunning) {
-            System.out.println("Enter your name (or type 'exit' to quit):");
+            System.out.println("ENTER YOUR NAME (or type 'exit' to quit):");
             String name = scanner.nextLine();
 
             if (name.equalsIgnoreCase("exit")) {
                 isRunning = false;
             } else {
-                System.out.println("Let the game begin!!!");
-
                 System.out.println("Enter the number from 0 to 100");
                 String userNumber = scanner.nextLine();
                 int parsedUserNumber = Integer.parseInt(userNumber);
                 int randomNumber = (int) (Math.random() * 101);
                 System.out.println(randomNumber);
+                userNumbersList.add(parsedUserNumber);
 
-                if(parsedUserNumber < randomNumber) {
+                if (parsedUserNumber < 0 || parsedUserNumber > 100) {
+                    System.out.println("Please enter a number between 0 and 100.");
+                    continue;
+                } else if(parsedUserNumber < randomNumber) {
                     System.out.println("Your number is too small. Please, try again.");
                 } else if (parsedUserNumber > randomNumber){
                     System.out.println("Your number is too big. Please, try again.");
-                }else{
-                    System.out.println("Congratulations, " + name);
+                } else {
+                    System.out.println("Congratulations, " + name + "!");
                 }
             }
         }
 
+        Collections.sort(userNumbersList);
+        System.out.println("Your entered numbers: " + userNumbersList);
+
         scanner.close();
     }
 }
+
+// чтоб запустить код, нужно сначала удалить файл класса, заново его скомпилировать и только потом запускается код
